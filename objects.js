9636aad9c2be760cd2b103d34caa56f57405b37a
Allison Tzadok <allisontzadok@gmail.com>
2017-12-04 18:47:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {artistName: 'song title'};
+var playlist = {artist name: 'song title'};
 
 function updatePlaylist({playlist}, {[artistName]: songTitle}){
   playlist.artistName = songTitle;
