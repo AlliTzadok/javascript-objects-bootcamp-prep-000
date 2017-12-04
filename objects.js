6d30ce7f325deb3d15342503a492cdac7e292e50
Allison Tzadok <allisontzadok@gmail.com>
2017-12-04 19:26:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {Three Days Grace: };
+var playlist = {Three Days Grace: 'Pain' };
 
 function updatePlaylist({playlist}, {artistName: songTitle}){
   playlist[artistName] = songTitle;
